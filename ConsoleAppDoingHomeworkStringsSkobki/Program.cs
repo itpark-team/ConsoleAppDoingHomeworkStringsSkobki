6a720d9180dfaabef99d80de0493d7a5e113ff6a
itpark-team <46758840+itpark-team@users.noreply.github.com>
2022-04-10 11:26:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppDoingHomeworkStringsSkobki/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDoingHomeworkStringsSkobki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr;
+            bool result = true;
+
+            Console.Write("введите выражение: ");
+            inputStr = Console.ReadLine();
+
+
+
+
+
+            if (result == true)
+            {
+                Console.WriteLine("скобки расставлены правильно");
+            }
+            else
+            {
+                Console.WriteLine("скобки расставлены НЕправильно");
+            }
+        }
+    }
+}
